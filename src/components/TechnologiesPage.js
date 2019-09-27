f353b512d0bf83997d16dcc8de13307327a9686a
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-09-27 15:38:42 -0600

    added logos and styling to technologies page

--- src/components/TechnologiesPage.js
+++ src/components/TechnologiesPage.js
@@ -1,29 +1,55 @@
 import React from 'react'
 
+import CSSIcon from '../images/CSS-Logo.png'
+import HTMLIcon from '../images/HTML-Logo.png'
+import JSIcon from '../images/JS-Logo.png'
+import NodeIcon from '../images/Node-Logo.png'
+import ReactIcon from '../images/React-Logo.png'
+import ReactNativeIcon from '../images/React-Native-Logo.png'
+import ReduxIcon from '../images/Redux-Logo.png'
+import SassIcon from '../images/Sass-Logo.png'
+import SQLIcon from '../images/SQL-Logo.png'
+
 import '../styling/TechnologiesPage.scss'
 
 function TechnologiesPage() {
     return(
         <div className='tech-page'>
-            <h1>Technologies I have experience with</h1>
+            <div className='title-container'>
+                <h1>Technologies I have experience with</h1>
+            </div>
             <div className='tech'>
                 <div>
-                    <h2>Front-End</h2>
-                    <ul>
-                        <li>html</li>
-                        <li>css/less/sass</li>
-                        <li>javascript</li>
-                        <li>React</li>
-                        <li>React Native</li>
-                        <li>Redux</li>
-                    </ul>
+                    <img src={HTMLIcon} />
+                    <p>HTML</p>
                 </div>
                 <div>
-                    <h2>Back-End</h2>
-                    <ul>
-                        <li>node.js</li>
-                        <li>mySQL</li>
-                    </ul>
+                    <img src={SassIcon} />
+                    <p>CSS/LESS/SASS</p>
+                </div>
+                <div>
+                    <img src={JSIcon} />
+                    <p>JavaScript</p>
+                </div>
+                <div>
+                    <img src={ReactIcon} />
+                    <p>React</p>
+                </div>
+                <div>
+                    <img src={ReactNativeIcon} />
+                    <p>React Native</p>
+                </div>
+                <div>
+                    <img src={ReduxIcon} />
+                    <p>Redux</p>
+                </div>
+                <div>
+                    <img src={NodeIcon} />
+                    <p>Node.js</p>
+                </div>
+                <div>
+                    <img src={SQLIcon} />
+                    <p>SQL</p>
                 </div>
             </div>
         </div>
