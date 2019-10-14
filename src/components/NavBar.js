5f50f02b78d1c633b8a17f2a128de82efe3dcb0e
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-10-14 14:57:39 -0600

    added scroll to function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,15 +3,23 @@ import React from "react";
 import "../styling/NavBar.scss";
 
 function NavBar() {
+
+    const scrollToTop = (num) => {
+        window.scrollTo({
+            top: num,
+            behavior: 'smooth'
+        })
+    }
+
   return (
-    <div className='nav-bar'>
-      <a href='#home'>Home</a>
-      <div className='site-nav'>
-        <a href='#technologies'>Technologies</a>
-        <a href='#projects'>Projects</a>
-        <a href='#contact'>Contact</a>
+      <div className='nav-bar'>
+        <a onClick={() => scrollToTop(0)}>Home</a>
+        <div className='site-nav'>
+          <a onClick={() => scrollToTop(1500)}>Technologies</a>
+          <a onClick={() => scrollToTop(2300)}>Projects</a>
+          <a onClick={() => scrollToTop(10000)}>Contact</a>
+        </div>
       </div>
-    </div>
   );
 }
 
