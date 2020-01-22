d599e04f78314e83ab25e7266d528461402b5b01
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2020-01-21 22:35:53 -0700

    able to randomly choose from array of background images on mount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // USE YARN
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styling/App.scss";
 
+// components
 import NavBar from "./components/NavBar";
 import TitlePage from "./components/TitlePage";
 import Projects from "./components/Projects";
@@ -10,11 +11,20 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import HiddenComponent from "./components/HiddenComponent";
 
-import background from "./images/backgrounds/midvale-mtn.JPG";
+//background images
+import midvaleMtn from "./images/backgrounds/midvale-mtn.JPG";
 
 function App() {
   const [toggle, setToggle] = useState(false);
   const [populateModal, setPopulateModal] = useState();
+  const [randomIndex, setRandomIndex] = useState();
+
+  // import background ↑ add it to useState array ↓
+  const background = useState([midvaleMtn]);
+
+  useEffect(() => {
+    setRandomIndex(Math.floor(Math.random() * background[0].length))
+  }, [background])
 
   const toggleModal = argument => {
     setToggle(true);
@@ -27,7 +37,7 @@ function App() {
 
   return (
     <div className='App'>
-      <img className='background' src={background} alt='background' />
+      <img className='background' src={background[0][randomIndex]} alt='background' />
       <NavBar />
       <TitlePage />
       <About />
